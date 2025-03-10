2402ccc8f9efd62af961f2934f63b13204d0cd34
Sahil Shefeek <sahilms345@gmail.com>
2025-03-10 22:25:56 +0530

    refactor AuthDependencies to use MultiRepositoryProvider and improve DI

--- lib/authentication/services/auth_dependencies.dart
+++ lib/authentication/services/auth_dependencies.dart
@@ -1,37 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 
 import '../../core/network/dio_service.dart';
 import '../bloc/authentication_bloc.dart';
 import 'auth_service.dart';
-import 'token_service.dart';
 
 class AuthDependencies extends StatelessWidget {
   final Widget child;
+  final DioService dioService;
 
-  const AuthDependencies({Key? key, required this.child}) : super(key: key);
+  const AuthDependencies({
+    super.key,
+    required this.child,
+    required this.dioService,
+  });
 
   @override
   Widget build(BuildContext context) {
-    final dioService = DioService();
     final tokenService = dioService.tokenService;
     final authService = AuthService(dioService.dioInstance, tokenService);
 
-    // Create the AuthBloc
-    final authBloc = AuthenticationBloc(authService)
-      ..add(AuthenticationStatusChecked());
-
-    // Set the authBloc in DioService
-    dioService.setAuthBloc(authBloc);
-
-    return MultiProvider(
-      providers: [
-        Provider<TokenService>.value(value: tokenService),
-        Provider<AuthService>.value(value: authService),
-        BlocProvider<AuthenticationBloc>.value(value: authBloc),
-      ],
-      child: child,
+    return MultiRepositoryProvider(
+      providers: [RepositoryProvider<DioService>.value(value: dioService)],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthenticationBloc>(
+            create: (context) {
+              final authBloc = AuthenticationBloc(authService)
+                ..add(AuthenticationStatusChecked());
+              dioService.setAuthBloc(authBloc);
+              return authBloc;
+            },
+          ),
+        ],
+        child: child,
+      ),
     );
   }
 }
